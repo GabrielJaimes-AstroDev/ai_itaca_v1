772b5d2a4879b83dd107c0812334160dcb42dd63
Gabriel Jaimes <bolivianvirtualobservatory@gmail.com>
2025-05-20 13:28:01 +0200

    Update aiitaca_streamlit_app.py

--- aiitaca_streamlit_app.py
+++ aiitaca_streamlit_app.py
@@ -271,7 +271,7 @@ A remarkable upsurge in the complexity of molecules identified in the interstell
 """, unsafe_allow_html=True)
 
 # === CONFIGURATION ===
-GDRIVE_FOLDER_URL = "https://drive.google.com/drive/folders/1PyAGVOum6MWE_2PDqysvrr_dg_acg-v8?usp=drive_link"
+GDRIVE_FOLDER_URL = "https://drive.google.com/drive/folders/1ng3gqWqPW9v7ZtbXHx6GLOHXj8fDGh8Q?usp=drive_link"
 TEMP_MODEL_DIR = "downloaded_models"
 
 if not os.path.exists(TEMP_MODEL_DIR):
