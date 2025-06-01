da85a6cbdf05340488eedbac03fa08d36e46b68b
Gabriel Jaimes <bolivianvirtualobservatory@gmail.com>
2025-06-01 14:56:59 +0200

    Update paths.py

--- paths.py
+++ paths.py
@@ -1,2 +1,2 @@
-GDRIVE_FOLDER_URL = "https://drive.google.com/drive/folders/1PyAGVOum6MWE_2PDqysvrr_dg_acg-v8?usp=drive_link"
+GDRIVE_FOLDER_URL = "https://drive.google.com/drive/folders/1zlnkEoRvHR1CoK9hXxD0Jy4JIKF5Uybzk"
 TEMP_MODEL_DIR = "downloaded_models"
