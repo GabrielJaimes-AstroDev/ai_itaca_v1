af089a68b560babc522e3fd4825d6df48848ca14
Gabriel Jaimes <bolivianvirtualobservatory@gmail.com>
2025-06-02 08:47:15 +0200

    Update aiitaca_streamlit_app.py

--- aiitaca_streamlit_app.py
+++ aiitaca_streamlit_app.py
@@ -205,7 +205,7 @@ config = {
     }
 }
 
-@st.cache_data(ttl=3600, max_entries=3, show_spinner="Loading ALMA cube...")
+#@st.cache_data(ttl=3600, max_entries=3, show_spinner="Loading ALMA cube...")
 
 # === PESTAÑAS PRINCIPALES ===
 tab_molecular, tab_cube = st.tabs(["Molecular Analyzer", "Cube Visualizer"])
